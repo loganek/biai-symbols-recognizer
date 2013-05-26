68feabc1c129f088ffd7002d9f29122c5db36feb
loganek <marcin.kolny@gmail.com>
2013-05-26 11:13:12 +0200

    dodalem cross-platformowosc

--- src/BIAI/BIAIShapes/BIAIShapes.cpp
+++ src/BIAI/BIAIShapes/BIAIShapes.cpp
@@ -2,15 +2,16 @@
 //
 #define _USE_MATH_DEFINES
 
+#ifdef _WIN32
 #include <tchar.h>
+#endif
 #include "MainProgram.h"
 
 using namespace std;
 
+#ifdef _WIN32
 int _tmain(int argc, _TCHAR* argv[])
 {
-	MainProgram program;
-
 	char** arguments = new char*[argc];
 
 	for ( int i = 0; i < argc; i++ )
@@ -20,15 +21,22 @@ int _tmain(int argc, _TCHAR* argv[])
 		arguments[i] = new char[str.length() + 1];
 		strcpy(arguments[i], str.c_str());
 	}
-	
+#elif __linux__
+int main (int argc, char* arguments[])
+{
+#endif
+
+	MainProgram program;
 	program.ParseArguments(argc, arguments);
-	
+
+#ifdef _WIN32
 	for ( int i = 0; i < argc; i++ )
 	{
 		delete [] arguments[i];
 	}
 
 	delete [] arguments;
+#endif
 
 	return 0;
 }
